227f28c1de1ef1c429f43d271c059b46a16481f9
Kalenhh <74684530+Kalenhh@users.noreply.github.com>
2023-01-16 15:13:57 +0100

    build test

---
+++ build.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+
+
+from setuptools import setup
+
+setup(
+	name = "musee",
+	options = {
+		'build_apps':{
+			'gui_apps' :{
+				'musee': 'main.py'
+			},
+
+		'include_patterns' : [
+			'**/*.gltf']
+		}
+	}
+)
