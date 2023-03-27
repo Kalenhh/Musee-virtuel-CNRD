780a6c83628a6aabd93b8f90c82aa4290f4a8ab1
Kalenhh <74684530+Kalenhh@users.noreply.github.com>
2023-03-27 21:52:57 +0200

    build try
    le build fonctionne

--- build.py
+++ build.py
@@ -1,15 +1,29 @@
-#coding:utf-8
-
-
 from setuptools import setup
 
 setup(
-	name = "musee",
-	options = {
-		'build_apps':{
-			'gui_apps' :{
-				'musee': 'main.py'
-			}
-		}
-	}
+    name='musee',
+    options={
+        'build_apps': {
+            'gui_apps': {
+                'musee': 'main.py',
+            },
+
+        
+
+            'include_patterns': [
+                '**/*.png',
+                '**/*.txt',
+                '**/*.wav'
+            ],
+
+            'plugins': [
+                'time',
+                'os',
+            ],
+
+            'platforms': ['win_amd64'],
+
+            'plugins': ['pandagl', 'p3openal_audio'],
+        }
+    }
 )
